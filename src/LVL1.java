90559b21ecd3c039cb6508ae2f78c32f0de3c7e3
Roman Kibenko <kiben81@mail.ru>
2024-11-13 22:34:16 +0300

    some changes

--- src/LVL1.java
+++ src/LVL1.java
@@ -4,7 +4,7 @@ public class LVL1 {
     public static void main(String[] args) {
         //Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
         //TODO Write you code here
-       /* while (true) {
+        while (true) {
         System.out.println("Введите число: ");
         int number = new Scanner(System.in).nextInt();
             if (number == 0) {
@@ -15,13 +15,14 @@ public class LVL1 {
             System.out.println("Вы ввели отрицательное число");
         } else System.out.println("Вы ввели положительное число");
 
-        }*/
+        }
 //Дана строка. Выведите в консоль длину этой строки.
         //TODO Write you code here
         while (true) {
             System.out.println("Введите строку: ");
             String string = new Scanner(System.in).nextLine();
             if (string.equalsIgnoreCase("stop")) {
+                System.out.println("Программа завершена!");
                 break;
             }
             System.out.println("Длина строки - " + string.length() + " символов");
