63026846143a78d7b4011ab8bb6b96ab21a14ecd
Roman Kibenko <kiben81@mail.ru>
2024-11-13 22:30:11 +0300

    init commit

---
+++ src/LVL1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+//Решение задач https://code.mu/ru/java/tasker/stager/1/1/
+public class LVL1 {
+    public static void main(String[] args) {
+        //Дано число. Проверьте, отрицательное оно или нет. Выведите об этом информацию в консоль.
+        //TODO Write you code here
+       /* while (true) {
+        System.out.println("Введите число: ");
+        int number = new Scanner(System.in).nextInt();
+            if (number == 0) {
+                System.out.println("Завершение программы!");
+                break;
+            }
+        if (number<0) {
+            System.out.println("Вы ввели отрицательное число");
+        } else System.out.println("Вы ввели положительное число");
+
+        }*/
+//Дана строка. Выведите в консоль длину этой строки.
+        //TODO Write you code here
+        while (true) {
+            System.out.println("Введите строку: ");
+            String string = new Scanner(System.in).nextLine();
+            if (string.equalsIgnoreCase("stop")) {
+                break;
+            }
+            System.out.println("Длина строки - " + string.length() + " символов");
+        }
+    }
+}
